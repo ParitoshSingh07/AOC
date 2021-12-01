dfef9870305e10fa73d7b79f0036647e3d04417d
Paritosh Singh <paritoshsingh1@kpmg.com>
2021-12-01 14:22:17 +0530

    day1 rust solution rough

---
+++ 2021/day01/script.rs
@@ -0,0 +1,51 @@
+use itertools::Itertools;
+
+fn main() {
+    let string = "
+line one
+line two
+".trim();
+    println!("{}", string);
+    println!("{:?}", string);
+    
+    let string = "
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+".trim();
+    let vec: Vec<i32> = string.split("\n").map(|x| x.parse::<i32>().unwrap()).collect();
+    println!("{:?}", vec);
+    // Part 1
+    /* let mut acc = 0;
+    for (prev, next) in vec.into_iter().tuple_windows() {
+        println!("{}--{}", prev, next);
+        if next > prev {
+            acc += 1;
+        }
+    }
+    println!("{}", acc); */
+    // Part 2
+
+    let mut vec2: Vec<i32> = Vec::new();
+    for (item1, item2, item3) in vec.into_iter().tuple_windows() {
+        vec2.push(item1 + item2 + item3);
+    };
+    println!("{:?}", vec2);
+    let mut acc = 0;
+    for (prev, next) in vec2.into_iter().tuple_windows() {
+        println!("{}--{}", prev, next);
+        if next > prev {
+            acc += 1;
+        }
+    }
+    println!("{}", acc);
+}
+
+
