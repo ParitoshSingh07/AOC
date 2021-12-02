c8241b5cd37f88f7818d76ae1dbfa4015b118e67
Paritosh Singh <paritoshsingh1@kpmg.com>
2021-12-02 13:27:12 +0530

    day 03 prep

---
+++ 2021/day03/script.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 13:26:03 2021
+
+@author: paritoshsingh1
+"""
+
+import sys
+sys.path.append(r"C:\paritosh\playground\AOC\AOC\2021")
+import os
+os.chdir(r"C:\paritosh\playground\AOC\AOC\2021\day03")
+from day01.day01 import sliding_window
+
+with open("input.txt") as f:
+    contents = f.read().strip()
+
+contents = """
+
+""".strip()
