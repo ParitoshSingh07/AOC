941732cb9b24700bed617d8e2ea63a2faf39543a
Paritosh_Singh <42860669+ParitoshSingh07@users.noreply.github.com>
2021-12-16 02:59:18 +0530

    day 9 partial - part 1

---
+++ 2021/day09/script.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 13:26:03 2021
+
+@author: paritoshsingh1
+"""
+
+import sys
+sys.path.append(r"..")
+from day01.day01 import sliding_window
+
+with open("input.txt") as f:
+    contents = f.read().strip()
+
+contents = """
+
+""".strip()
